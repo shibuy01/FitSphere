0e88de9bf07b06e9c22dfeb604110bb1c9532d80
Shibu_01 <shibuy0011@gmail.com>
2026-09-20 00:30:25 +0530

    ai_Service_Improve

---
+++ aiService/src/main/java/com/fitness/aiService/service/RecommendationService.java
@@ -0,0 +1,26 @@
+package com.fitness.aiService.service;
+
+import com.fitness.aiService.models.Recommendation;
+import com.fitness.aiService.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() ->
+                        new RuntimeException("No Recommendation found for this activity")
+                );
+    }
+}
